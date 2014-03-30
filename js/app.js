546e62cd6ef83bd58ecd7c18328ff325dce9d101
Michael Seiwald <michael@mseiwald.at>
2014-03-30 23:09:26 +0200

    initialize track information from mopidy

--- js/app.js
+++ js/app.js
@@ -27,3 +27,26 @@ Mops.IndexController = Ember.ObjectController.extend({
 });
 
 Mops.mopidy = new Mopidy();
+
+Mops.mopidy.on("state:online", function() {
+    Mops.mopidy.playback.getCurrentTrack().then(function(track) {
+        Mops.set('model.currentTrack.artist', track.artists[0].name);
+        Mops.set('model.currentTrack.title', track.name);
+    });
+    Mops.mopidy.playlists.getPlaylists().then(function(playlists) {
+        playlists.map(function(playlist) {
+            Mops.model.playlists.addObject(playlist.name);
+        });
+    });
+    Mops.mopidy.tracklist.getTracks().then(function(tracklist) {
+        Mops.tracklist_orig = tracklist;
+        tracklist.map(function(track) {
+            Mops.model.tracklist.addObject({
+                "artist": track.artists[0].name,
+                "title": track.name,
+                "album": track.album.name,
+                "duration": millisecondsToTime(track.length)
+            });
+        });
+    });
+});
