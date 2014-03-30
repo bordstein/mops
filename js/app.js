f0738c1672e6bcdf530d71bd305b46df8c67cfff
Michael Seiwald <michael@mseiwald.at>
2014-03-30 23:05:05 +0200

    remove dummy data

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 Mops = Ember.Application.create();
 
 Mops.model = {
-  "currentTrack": {"artist": "Hans Tester", "title": "Once in a lifetime"},
-  "tracklist": [{"track": 1, "artist": "Hans Tester", "title": "Once in a lifetime", "album": "Best of Kasnockn", "duration": "03:20"},
- {"track": 2, "artist": "Hans Tester", "title": "Ooooh baby", "album": "Best of Kasnockn", "duration": "02:50"} ],
-  "playlists": [{"name": "Rock'n'Roll"},
-    {"name": "Best of 60s"},{"name": "happy, happy"}]
+    "currentTrack": {
+        "artist": "",
+        "title": ""
+    },
+    "tracklist": [],
+    "playlists": []
 }
 
 Mops.Router.map(function() {
