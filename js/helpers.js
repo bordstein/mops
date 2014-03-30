b69b8b6d63cd0cf5043324e20ed37fc9602963d6
Michael Seiwald <michael@mseiwald.at>
2014-03-30 22:53:58 +0200

    add helper function millisecondsToTime()

---
+++ js/helpers.js
@@ -0,0 +1,7 @@
+function millisecondsToTime(milli) {
+    var seconds = parseInt((milli / 1000) % 60);
+    var minutes = parseInt((milli / (60 * 1000)) % 60);
+    if (seconds.toString().length == 1)
+        seconds = '0' + seconds;
+    return minutes + ":" + seconds;
+}
